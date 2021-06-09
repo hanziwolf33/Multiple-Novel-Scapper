e0fec83ce0aa027b12ddf5de6fcf095dfb03da36
hanziwolf33 <85340328+hanziwolf33@users.noreply.github.com>
2021-06-09 14:28:48 -0400

    Add files via upload

--- 5. Multipdf to pdf.py
+++ 5. Multipdf to pdf.py
@@ -1,7 +1,7 @@
 import os, PyPDF2, requests, glob, shutil
 from unidecode import unidecode
 
-global baseDIR, baseDIRx, ListName, num, Name
+global baseDIR, baseDIRx, ListName, num, Name, numx, LocationPDF
 baseDIR = 'D:/Novels/PDF Chapter Novels'
 baseDIRx = 'D:/Novels/PDF Book Novels'
 DIR = '.'
@@ -16,16 +16,16 @@ File.close()
 Name = ListName.pop(-1)
 files = glob.glob(baseDIR+'/'+Name + '/*.pdf')
 files.sort(key=os.path.getmtime)
-num = len(files)
+numx = len(files)
+num = len(ListName)
 
 while files:
   if num != 0:
     print('Remaining novels to be converted into book style: '+str(num))
     ListPDF = files.pop(-1)
-    global LocationPDF
-    LocationPDF = ListPDF
-    LPDF = LocationPDF
-    userpdflocation=baseDIR+'/'+Name
+    LocationPDF = str(ListPDF)
+    LPDF = str(LocationPDF)
+    userpdflocation=str(baseDIR+'/'+Name)
     os.chdir(userpdflocation)
     userfilename=Name.replace(' pdf','')
     print('Creating pdf book style version of: '+userfilename)
@@ -33,12 +33,13 @@ while files:
     pdf2merge = []
     for filename in os.listdir('.'):
       x+=1
-      if filename.endswith('.pdf'):
-        if filename == str(x)+'.pdf':
-          pdf2merge.append(filename)
-        else:
-          filename = str(x)+'.pdf'
-          pdf2merge.append(filename)
+      if x != numx:
+        if filename.endswith('.pdf'):
+          if filename == str(x)+'.pdf':
+            pdf2merge.append(filename)
+          else:
+            filename = str(x)+'.pdf'
+            pdf2merge.append(filename)
 
     pdfWriter = PyPDF2.PdfFileWriter()
 
@@ -48,9 +49,10 @@ while files:
       for pageNum in range(pdfReader.numPages):
         pageObj = pdfReader.getPage(pageNum)
         pdfWriter.addPage(pageObj)
-        pdfOutput = open(baseDIRx+userfilename+'.pdf', 'wb')
-    pdfWriter.write(baseDIRx+pdfOutput)
+        pdfOutput = open(baseDIRx+'/'+userfilename+'.pdf', 'wb')
+    pdfWriter.write(pdfOutput)
     pdfOutput.close()
-    shutil.move(pdfOutput, baseDIRx)
+    #shutil.move(pdfOutput, baseDIRx)
     print('Finished pdf book style version of: '+userfilename)
+    Name = ListName.pop(-1)
   num-=1
